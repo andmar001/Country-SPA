e77a8ffc96f3c15f924cc4df29f07c482ceece03
tony tolver <andrademaresantonio00@gmail.com>
2023-07-31 03:13:03 -0600

    Routing

---
+++ src/app/shared/pages/about-pages/about-pages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-about-page',
+  templateUrl: './about-pages.component.html',
+  styles: [
+  ]
+})
+export class AboutPagesComponent {
+
+}
