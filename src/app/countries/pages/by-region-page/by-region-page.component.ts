4447adca9f63065cd702ee6caf076ae83ca15894
tony tolver <andrademaresantonio00@gmail.com>
2023-08-27 23:59:26 -0600

    Optimizacion buscar por región

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -13,9 +15,15 @@ export class ByRegionPageComponent {
   placeholder = 'Buscar por región...';
   public countries:Country[] = [];
 
+  public regions:Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion? : Region;
+
   constructor( private _countriesService:CountriesService) { }
 
-  searchByRegion( region: string ):void {
+  searchByRegion( region: Region ):void {
+
+    this.selectedRegion = region;
+
     this._countriesService.searchByRegion( region )
       .subscribe( countries => {
         this.countries = countries;
